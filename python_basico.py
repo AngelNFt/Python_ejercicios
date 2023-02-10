950d261d555fcbf5a1323cf550548d5969bfce61
Angel Adrian de la Rosa Rosario <98406166+AngelNFt@users.noreply.github.com>
2023-02-10 06:24:31 -0600

    Add files via upload

---
+++ python_basico.py
@@ -0,0 +1,24 @@
+#Comentarios de una sola linea
+'''
+
+Comentarios largos en python
+Con varias lineas
+'''
+
+#Crear una variable con string
+string = "pedro"
+listas = [2,3,6,7]
+
+booleanos_falsos = False
+booleanos_verdaderos = True
+
+numero_flotante = 4.6
+
+#crear una variable con numeros
+numero_entero = 2
+
+diccionarios = {"juan": 23, "pedro": 14, "maria": 12}
+
+# opraciones suma, resta, multiplicacion, division(/.//),modulo (%),potencial()
+
+print(string * numero_entero)
